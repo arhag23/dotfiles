1553864fc5bb060f5968fefe04ab37c25ff85fee
arhag23 <agatram2023@gmail.com>
2025-03-17 00:56:17 -0400

    updated bat, fish, hypr configs, downloaded some fonts

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    oh-my-posh init fish --config ~/.config/omp/config.omp.json | source
     set -g fish_term24bit 1
     set -g theme_color_scheme nord
     set -g theme_date_format "+%b %d %r"
